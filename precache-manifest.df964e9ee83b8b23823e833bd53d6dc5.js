2840f11214232575724cd2463cad95e63acfc1f7
kelbikhanov <rusik9091@gmail.com>
2020-02-26 07:02:59 +0300

    Updates

---
+++ precache-manifest.df964e9ee83b8b23823e833bd53d6dc5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efcb1ed7127a2e78f4f9a46df8d3ec98",
+    "url": "/Kelbikhanov/Weather/index.html"
+  },
+  {
+    "revision": "8ecaf8d2c75ae0fa81fb",
+    "url": "/Kelbikhanov/Weather/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "a5d39cd880c63f615f06",
+    "url": "/Kelbikhanov/Weather/static/css/main.abff06eb.chunk.css"
+  },
+  {
+    "revision": "8ecaf8d2c75ae0fa81fb",
+    "url": "/Kelbikhanov/Weather/static/js/2.13d7c231.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Kelbikhanov/Weather/static/js/2.13d7c231.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a5d39cd880c63f615f06",
+    "url": "/Kelbikhanov/Weather/static/js/main.28a4c7ed.chunk.js"
+  },
+  {
+    "revision": "fec63d1956f3ed4e85bd",
+    "url": "/Kelbikhanov/Weather/static/js/runtime-main.ffd4f45a.js"
+  },
+  {
+    "revision": "cc54c19b29890841267ee0750eb25a00",
+    "url": "/Kelbikhanov/Weather/static/media/img1.cc54c19b.jpg"
+  }
+]);
